4ceb955263cd6ea099453a694fea6793ef0067fb
thomasdekerf <62058195+thomasdekerf@users.noreply.github.com>
2025-09-11 13:57:01 +0200

    Add virtual staining dataset support

--- model/utils/dataset.py
+++ model/utils/dataset.py
@@ -3,6 +3,8 @@ import torch.utils.data as data
 from torchvision import transforms
 import random
 import os
+from typing import List
+
 
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
@@ -18,30 +20,66 @@ def pil_img_loader(path):
     else:
         return img
 
+
+def _read_txt_list(list_path: str, root: str = "") -> List[str]:
+    image_list: List[str] = []
+    with open(list_path, "r") as f:
+        for line in f:
+            image_list.append(os.path.join(root, line.strip()))
+    return image_list
+
+
+def _scan_dir(dir_path: str) -> List[str]:
+    files = sorted(os.listdir(dir_path))
+    return [
+        os.path.join(dir_path, f)
+        for f in files
+        if os.path.splitext(f)[1] in IMG_EXTENSIONS
+    ]
+
+
 class BaseDataset(data.Dataset):
     def __init__(self, cfg):
-        src_list = cfg.source_list
-        src_root = cfg.source_root
-        tgt_list = cfg.target_list
-        tgt_root = cfg.target_root
-        self.src_list = []
-        self.tgt_list = []
-        with open(src_list, 'r') as f:
-            for line in f.readlines():
-                self.src_list.append(os.path.join(src_root,line.strip()))
-        with open(tgt_list, 'r') as f:
-            for line in f.readlines():
-                self.tgt_list.append(os.path.join(tgt_root,line.strip()))
+        src_list = getattr(cfg, "source_list", None)
+        src_root = getattr(cfg, "source_root", "")
+        src_dir = getattr(cfg, "source_dir", None)
+        tgt_list = getattr(cfg, "target_list", None)
+        tgt_root = getattr(cfg, "target_root", "")
+        tgt_dir = getattr(cfg, "target_dir", None)
+
+        self.src_list: List[str] = []
+        self.tgt_list: List[str] = []
+
+        if src_dir:
+            self.src_list = _scan_dir(src_dir)
+        elif src_list:
+            self.src_list = _read_txt_list(src_list, src_root)
+        else:
+            raise ValueError("source_dir or source_list must be specified.")
+
+        if tgt_dir:
+            self.tgt_list = _scan_dir(tgt_dir)
+        elif tgt_list:
+            self.tgt_list = _read_txt_list(tgt_list, tgt_root)
+        else:
+            raise ValueError("target_dir or target_list must be specified.")
+
         self.src_len = len(self.src_list)
         self.tgt_len = len(self.tgt_list)
 
         transform_options = cfg.transform
         height, width = cfg.height, cfg.width
-        scale_lower,scale_higher = cfg.scale_l, cfg.scale_h
+        scale_lower, scale_higher = cfg.scale_l, cfg.scale_h
         transform_list = []
-        
+
         if 'random_resized_crop' in transform_options:
-            transform_list.append(transforms.RandomResizedCrop((height, width), scale=(scale_lower, scale_higher), ratio=(0.75, 1.3333333333333333)))
+            transform_list.append(
+                transforms.RandomResizedCrop(
+                    (height, width),
+                    scale=(scale_lower, scale_higher),
+                    ratio=(0.75, 1.3333333333333333),
+                )
+            )
         else:
             transform_list.append(transforms.Resize((height, width)))
         if 'crop' in transform_options:
@@ -52,7 +90,9 @@ class BaseDataset(data.Dataset):
             transform_list.append(transforms.RandomVerticalFlip(0.5))
         transform_list.append(transforms.ToTensor())
         if 'normalize' in transform_options:
-            transform_list.append(transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]))
+            transform_list.append(
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            )
         self.transform = transforms.Compose(transform_list)
         self.image_loader = pil_img_loader
         self.random_pair = cfg.random_pair
@@ -60,23 +100,30 @@ class BaseDataset(data.Dataset):
 
     def __getitem__(self, index):
         if self.random_pair:
-            tgt_idx = random.randint(0, self.tgt_len-1)
+            tgt_idx = random.randint(0, self.tgt_len - 1)
         else:
             tgt_idx = index % self.tgt_len
         src_img = pil_img_loader(self.src_list[index])
         tgt_img = pil_img_loader(self.tgt_list[tgt_idx])
         src_img = self.transform(src_img)
         tgt_img = self.transform(tgt_img)
-        
+
         if not self.return_name:
             return src_img, tgt_img
         else:
-            return src_img, tgt_img, '{}_to_{}.png'.format(self.src_list[index].split('/')[-1].split('.')[0], self.tgt_list[tgt_idx].split('/')[-1].split('.')[0])
+            return (
+                src_img,
+                tgt_img,
+                '{}_to_{}.png'.format(
+                    os.path.splitext(os.path.basename(self.src_list[index]))[0],
+                    os.path.splitext(os.path.basename(self.tgt_list[tgt_idx]))[0],
+                ),
+            )
 
     def __len__(self):
         return self.src_len
 
 
 def get_dataset(cfg):
-    dataset = BaseDataset(cfg)
-    return dataset
+    return BaseDataset(cfg)
+
